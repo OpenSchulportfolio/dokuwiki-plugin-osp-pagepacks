3dc5c73759139a622c7cdcfe80851add9ccd096e
Frank Schiebel <frank@linuxmuster.net>
2012-03-10 00:49:44 +0100

    teständerung mit neuem github username

--- conf/metadata.php
+++ conf/metadata.php
@@ -3,9 +3,7 @@
  * Metadata for configuration manager plugin
  * Additions for the filelist plugin
  *
- * @author Gina Haeussge <osd@foosel.net>
+ * @author Frank Schiebel <frank@linuxmuster.net>
  */
 
-$meta['allowed_absolute_paths'] = array('');
-$meta['web_paths'] = array('');
 $meta['pagepacks_src'] = array('');
